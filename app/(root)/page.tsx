dc69928570e86b16ae78ce80083dd5828d4e7f73
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2025-06-07 03:59:47 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,46 +1,12 @@
-import { Suspense } from "react";
-import ProdhanSection from "@/components/prodhan-section";
-import GovernmentSchemeSection from "@/components/government-scheme-section";
 
-import HeroSection from "@/components/hero-section";
-import AdminMarquee from "@/components/AdminMarquee";
-
-import { db } from "@/lib/db";
-import Image from "next/image";
 import Link from "next/link";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import {
-  Calendar,
-  Users,
-  MapPin,
-  Phone,
-  Mail,
-  ExternalLink,
-} from "lucide-react";
-import LatestNewsUpdate from "@/components/latest-news-update";
-import { AdUnit } from "@/components/adsense-provider";
+
 import { auth } from "@/auth";
-type AdminMessage = {
-  id: string;
-  title: string;
-  content: string;
-  bgColor: string;
-  textColor: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
+
 
 export default async function Home() {
-  const addminmessage = (await db.adminMessage.findMany({})) as AdminMessage[];
-
+  
   const session = await auth();
   if (!session) {
     return (
